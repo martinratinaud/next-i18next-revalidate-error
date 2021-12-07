8ac72a2252706f2dfb06e522f96b5aa1f874a8f9
Martin Ratinaud <martinratinaud@gmail.com>
2021-12-07 12:16:17 +0400

    Add next-i18next.config.js

--- modules/I18n/next-i18next.config.js
+++ modules/I18n/next-i18next.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-    defaultNS: 'common',
+  defaultNS: 'common',
   i18n: {
     defaultLocale: 'en',
     locales: ['en','fr'],
     localePath: "translations",
     reloadOnPrerender: true,
   },
-  };
+  // ns:["common"],
+  // https://github.com/vercel/next.js/issues/22508
+  react: {
+    useSuspense: false,
+  },
+};
---
+++ next-i18next.config.js
@@ -0,0 +1,1 @@
+module.exports = require('./modules/I18n/next-i18next.config');
