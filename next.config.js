5d9c600fa3daa7098dd4a4404407a7640ad0c4c5
Martin Ratinaud <martinratinaud@gmail.com>
2021-12-07 12:05:45 +0400

    add staticpagegenerationtimeout

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require('./modules/I18n/next-i18next.config');
 
 module.exports = {
   i18n,
+  staticPageGenerationTimeout: 360,
   reactStrictMode: true,
 };
 
